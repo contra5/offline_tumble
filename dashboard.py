2e8c7dee059c6787a7ae8fdb26c289de3a822515
contra5 <45379913+contra5@users.noreply.github.com>
2018-11-26 23:21:42 -0500

    Intial commit

---
+++ dashboard.py
@@ -0,0 +1,318 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+
+import bs4
+import numpy as np
+
+import os
+import os.path
+import json
+import random
+
+import tumdis
+import dash_reusable_components as drc
+
+
+app = dash.Dash(__name__)
+
+#app.config['suppress_callback_exceptions']=True
+
+
+AC = tumdis.AccountCollection(inDash = True)
+
+startingAccountName = AC[random.randint(0, len(AC) - 1)].name
+
+
+#bad form, but works in single user system
+num_nexts = 0
+num_prevs = 0
+current_post_num = 0
+
+def genPostSelectorMarks(A):
+    maxVal = len(A) - 1
+    if maxVal >= 10:
+        #vals =  {(i * maxVal) // 10 : f"{(i * maxVal) // 10}" for i in range(10)}
+        #print(vals)
+        return {int(i) : str(i) for i in np.linspace(0, maxVal , 10, dtype = int)}
+    else:
+        return {i : f"{i}" for i in range(maxVal)}
+
+
+def genTagOptions(A, withNum = True):
+    ret = [{'value' : 'None', 'label' : f'{len(A)} None'}]
+    for c, t in A.sortedTags(countTuple = True):
+        ret.append({
+            'value' : t,
+            'label' : f"{c} {t}" if  withNum else t,
+        })
+    return ret
+
+def genTypesDict(A):
+    return [{'value' : t, 'label' : t } for t in A.postTypes]
+
+app.layout = html.Div([
+    html.Div(id='state_container', style={'display': 'none'},
+                **{'data-account' : startingAccountName,
+                    'data-post' : 0,
+                    'data-type' : 'texts',
+                    'data-tag' : 'None',
+                    'data-buttons' : (0, 0, 0)}),
+    html.Div([
+        drc.NamedDropdown(
+            name = 'Account',
+            id = 'account_selector',
+            options=[{'value' : n, 'label' : n} for n in AC.names],
+            value=startingAccountName,
+        ),
+        drc.NamedDropdown(
+            name = 'Tags',
+            id = 'tags_selector',
+            options=genTagOptions(AC[startingAccountName]),
+            value='None',
+        ),
+        drc.NamedSlider(
+            name = 'Post',
+            id = 'post_selector',
+            min = 0,
+            max = len(AC[startingAccountName]) - 1,
+            step = 1,
+            value = 0,
+            marks = genPostSelectorMarks(AC[startingAccountName]),
+            updatemode = 'mouseup',
+        ),
+        drc.NamedRadioItems(
+            name = 'Type',
+            id = 'type_selector',
+            options = genTypesDict(AC[startingAccountName]),
+            value = 'texts' if 'texts' in AC[startingAccountName].postTypes else AC[startingAccountName].postTypes[0],
+        ),
+        html.Button(id='previous_button', n_clicks=0, children='Previous'),
+        html.Button(id='next_button', n_clicks=0, children='Next'),
+        drc.NamedDropdown(
+            name = 'Current Tags',
+            id = 'current_tags',
+            options=genTagOptions(AC[startingAccountName], withNum = False),
+            value=AC[startingAccountName][0].tags,
+            multi = True,
+        ),
+        html.Div(
+            style={'margin': '10px 0px'},
+            children=[
+                html.P(
+                    children='url:',
+                    style={'margin-left': '3px'}
+                ),
+                html.Div(
+                html.A(AC[startingAccountName][0].get('Post url', ''),
+                    href=AC[startingAccountName][0].get('Post url', ''))
+                    , id = 'post_url'),
+            ]
+        ),
+    ], className = 'sidenav'),
+    html.Div([
+        AC[startingAccountName](0),
+    ], id = 'tumblr_entry')
+])
+
+
+#State
+
+@app.callback(
+    Output('state_container', 'data-account'),
+    [Input('account_selector', 'value'),],
+)
+def update_post_value(new_account):
+    print(f"new_account {new_account}")
+    return new_account
+
+@app.callback(
+    Output('state_container', 'data-type'),
+    [Input('type_selector', 'value'),],
+)
+def update_post_value(new_type):
+    print(f"new_type {new_type}")
+    return new_type
+
+@app.callback(
+    Output('state_container', 'data-tag'),
+    [Input('tags_selector', 'value')],
+)
+def update_tag_value(new_tag):
+    print(f"new_tag {new_tag}")
+    return new_tag
+
+@app.callback(
+    Output('state_container', 'data-buttons'),
+    [Input('state_container', 'data-account'),
+    Input('next_button', 'n_clicks'),
+    Input('previous_button', 'n_clicks'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),],
+    [State('state_container', 'data-buttons'),
+    State('state_container', 'data-post')],
+)
+def update_post_index(account, next_presses, previous_presses, postType, postTag, button_state, currentPost):
+    print("post update press: ", next_presses, previous_presses, *button_state)
+    postIndex, num_nexts, num_prevs  = button_state
+    postIndex = currentPost
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    if next_presses > num_nexts:
+        num_nexts = num_nexts + 1
+        if postIndex < len(AC[account]) - 1:
+            postIndex =  postIndex + 1
+        else:
+            postIndex = 0
+    elif previous_presses > num_prevs:
+        num_prevs = num_prevs + 1
+        if postIndex > 0:
+            postIndex = postIndex - 1
+        else:
+            postIndex =  len(AC[account]) - 1
+    else:
+        postIndex = 0
+    return (postIndex, num_nexts, num_prevs)
+
+@app.callback(
+    Output('state_container', 'data-post'),
+    [Input('post_selector', 'value'),],
+)
+def update_post_value(new_post):
+    print(f"new_post {new_post}")
+    return new_post
+
+#Draw
+
+@app.callback(
+    Output('tumblr_entry', 'children'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),
+    Input('state_container', 'data-post'),],
+)
+def update_output_div(account, postType, postTag, postNum):
+    print("Draw:", account, postType, postTag, postNum)
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    return AC[account](postNum)
+
+@app.callback(
+    Output('post_url', 'children'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),
+    Input('state_container', 'data-post'),],
+)
+def update_output_div(account, postType, postTag, postNum):
+    print("Draw:", account, postType, postTag, postNum)
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    return html.A(AC[account][postNum].get('Post url', ''),
+        href=AC[account][postNum].get('Post url', ''))
+
+
+#Options
+
+
+@app.callback(
+    Output('type_selector', 'options'),
+    [Input('state_container', 'data-account'),]
+)
+def update_type_options(account):
+    return genTypesDict(AC[account])
+
+@app.callback(
+    Output('tags_selector', 'options'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),]
+)
+def update_tags_options(account, aType):
+    print(f"tags {AC[account].sortedTags(countTuple = True)[:5]}")
+    return genTagOptions(AC[account])
+
+@app.callback(
+    Output('current_tags', 'options'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),
+    Input('state_container', 'data-post'),],
+)
+def update_output_tag_options(account, postType, postTag, postNum):
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    print("Draw tags:", AC[account][postNum].tags)
+    return  genTagOptions(AC[account], withNum = False)
+
+
+@app.callback(
+    Output('post_selector', 'max'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),]
+)
+def update_post_max(account, postType, postTag):
+    print("max", len(AC[account]) - 1)
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    return len(AC[account]) - 1
+
+@app.callback(
+    Output('post_selector', 'marks'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),]
+)
+def update_post_marks(account, postType, postTag):
+    print("marks", genPostSelectorMarks(AC[account]))
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    return genPostSelectorMarks(AC[account])
+
+#Values
+
+@app.callback(
+    Output('type_selector', 'value'),
+    [Input('state_container', 'data-account'),],
+    [State('type_selector', 'options')]
+)
+def update_type_value(account, options):
+    print("zeroing types", options)
+    avail = [t['value'] for t in options]
+    return 'texts' if 'texts' in avail else avail[0]
+
+@app.callback(
+    Output('tags_selector', 'value'),
+    [Input('state_container', 'data-account'),],
+)
+def update_tag_value(account):
+    print("zeroing tags")
+    return 'None'
+
+@app.callback(
+    Output('current_tags', 'value'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-type'),
+    Input('state_container', 'data-tag'),
+    Input('state_container', 'data-post')],
+)
+def update_output_tag_options(account, postType, postTag, postNum):
+    AC[account].currentType = postType
+    AC[account].currentTag = postTag
+    print("Draw tags:", AC[account][postNum].tags)
+    return  AC[account][postNum].tags
+
+@app.callback(
+    Output('post_selector', 'value'),
+    [Input('state_container', 'data-account'),
+    Input('state_container', 'data-buttons')],
+)
+def update_post_value(account, button_vals):
+    print("updating post: ",  button_vals[0])
+    return button_vals[0]
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ tumdis.py
@@ -0,0 +1,376 @@
+import os
+import os.path
+import re
+import time
+import random
+import base64
+
+import collections.abc
+
+import bs4
+import dateutil.parser
+from IPython.core.display import display
+from ipywidgets import widgets, HBox, VBox
+
+import dash_html_components as html
+
+sizedRE = re.compile(r'_\d\d\d\d?[.]')
+
+tumblrsPath = 'downloads/'
+
+def encode_image(path):
+    extension = path.split('.')[-1]
+    with open(path, 'rb') as f:
+        return 'data:image/{};base64,{}'.format(extension,base64.b64encode(f.read()).decode('utf-8'))
+        S
+
+unknowns = ['autoplay', 'allow', 'align', 'border', 'imageanchor']
+
+def filterAttributes(a):
+    ret = {}
+    for k, v in a.items():
+        if k == 'class':
+            k = 'className'
+        elif k in unknowns:
+            continue
+        ret[k] = v
+    return ret
+
+def toDashHTML(t):
+    children = []
+    if isinstance(t, bs4.NavigableString):
+        return str(t)
+    else:
+        for c in t.children:
+            children.append(toDashHTML(c))
+    return getattr(html, t.name.title(), html.Div)(children, **filterAttributes(t.attrs))
+
+class DashDis(object):
+    def __init__(self, path = tumblrsPath):
+        self.path = path
+        self.AC = AccountCollection(path, inDash = True)
+        self.A = self.AC[random.randint(0, len(self.AC))]
+
+class TumDisplay(object):
+    def __init__(self, path = tumblrsPath):
+        self.path = path
+        self.AC = AccountCollection(path)
+        self.A = self.AC[random.randint(0, len(self.AC))]
+
+
+        self.accountSelector  = widgets.Dropdown(
+                    options=self.AC.names,
+                    value=self.A.name,
+                    description='Account:',
+                    )
+
+        self.tagsSelector = widgets.Dropdown(
+            options=[f'{len(self.A)} None'] + self.A.sortedTags(count = True),
+            value=f'{len(self.A)} None',
+            description='tags:',
+        )
+
+        self.typesSelector = widgets.RadioButtons(
+                options=self.A.postTypes,
+                value = self.A.currentType,
+                description='Post type:',
+        )
+
+        self.postSlider = widgets.IntSlider(min=0, max=len(self.A) - 1, value=0, description=str(len(self.A)) + ' posts')
+
+        self.mainCaption = widgets.HTML(
+            value=self.A(0)
+        )
+
+        self.nextButton = widgets.Button(description="Next")
+
+        self.previousButton = widgets.Button(description="Previous")
+
+        self.tagSearchEnable = widgets.Button(description="Enable tag search")
+
+        self.globalTagSelector = widgets.RadioButtons(
+                options=['None'],
+                value = 'None',
+                description='Global tag filter:',
+                disabled = True,
+                display = False,
+        )
+
+        self.controls = HBox([VBox([HBox([self.accountSelector, self.tagsSelector]), HBox([self.postSlider, self.previousButton, self.nextButton])]), self.typesSelector])
+
+
+    def account_change(self, change):
+        self.A = self.AC[self.accountSelector.value]
+        self.A.currentTag = 'None'
+        self.mainCaption.value = self.A(0)
+        self.postSlider.value = 0
+        self.postSlider.max=len(self.A) - 1
+        self.postSlider.description = str(len(self.A)) + ' posts'
+        self.tagsSelector.options= [f'{len(self.A)} None'] + self.A.sortedTags(count = True)
+        self.tagsSelector.value = f'{len(self.A)} None'
+
+        self.typesSelector.options = self.A.postTypes
+        self.typesSelector.value = 'texts'
+
+    def tag_change(self, change):
+        self.A.currentTag = ' '.join(self.tagsSelector.value.split(' ')[1:])
+        self.mainCaption.value = self.A(0)
+        self.postSlider.value = 0
+        self.postSlider.max=len(self.A) - 1
+        self.postSlider.description = str(len(self.A)) + ' posts'
+
+    def type_change(self, value):
+        #if  self.typesSelector.value not in ['texts', 'images']:
+        #    self.A.currentType = 'texts'
+        #else:
+        self.A.currentType = self.typesSelector.value
+        self.tagsSelector.options= [f'{len(self.A)} None'] + self.A.sortedTags(count = True)
+        self.tagsSelector.value = f'{len(self.A)} None'
+        self.tag_change('None')
+
+    def slider_change(self, change):
+        self.mainCaption.value = self.A(self.postSlider.value)
+
+    def go_next(self, b):
+        if self.postSlider.value < len(self.A) - 1:
+            self.postSlider.value += 1
+        else:
+            self.postSlider.value = 0
+        self.mainCaption.value = self.A(self.postSlider.value)
+
+    def go_previous(self, b):
+        if self.postSlider.value > 0:
+            self.postSlider.value -= 1
+        else:
+            self.postSlider.value = len(self.A) - 1
+        self.mainCaption.value = self.A(self.postSlider.value)
+
+    def enable_global_tags(self, b):
+        self.tagSearchEnable.disabled = True
+        self.tagSearchEnable.layout.display = False
+        self.globalTagSelector.disabled = False
+        self.globalTagSelector.layout.display = True
+
+    def _ipython_display_(self):
+        display(self.controls)
+        time.sleep(.1)
+        self.accountSelector.observe(self.account_change, names='value')
+
+        self.tagsSelector.observe(self.tag_change, names='value')
+
+        self.typesSelector.observe(self.type_change, names='value')
+
+        self.postSlider.observe(self.slider_change, names='value')
+
+        self.nextButton.on_click(self.go_next)
+        self.previousButton.on_click(self.go_previous)
+
+        self.tagSearchEnable.on_click(self.enable_global_tags)
+
+        display(self.mainCaption, self.controls)
+
+    def display(self):
+        return display(self)
+
+class AccountCollection(collections.abc.Mapping):
+    def __init__(self, path = tumblrsPath, inDash = False):
+        self.path = path
+        self.names = listAccounts(self.path)
+        self.loaded = {}
+        self.inDash = inDash
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self[self.names[key]]
+        if key not in self.loaded:
+            try:
+                self.loaded[key] = Account(key, inDash = self.inDash)
+            except FileNotFoundError:
+                raise KeyError
+        return self.loaded[key]
+
+    def __iter__(self):
+        for n in self.names:
+            yield n
+        return
+
+    def __len__(self):
+        return len(self.names)
+
+    def __repr__(self):
+        return f"< AccountCollection {len(self)} accounts {len(self.loaded)} loaded >"
+
+    def load_all(self):
+        for n in self:
+            a = self[n]
+
+    def tags(self):
+        tags = {}
+        for n in self:
+            a = self[n]
+            for t in a.sortedTags()[:20]:
+                try:
+                    tags[t] += len(a.tags[t])
+                except KeyError:
+                    tags[t] = len(a.tags[t])
+        return tags
+
+class Entry(object):
+    def __init__(self, dat):
+        self.dat = dat
+
+    def __getitem__(self, key):
+        return self.dat[key]
+
+    def get(self, key, *args, **kwargs):
+        return self.dat.get(key,*args, **kwargs)
+
+    def _repr_html_(self):
+        return self.html
+
+    def __str__(self):
+        return self['text']
+
+    @property
+    def html(self):
+        s = ''
+        if 'Photo url' in self.dat:
+            if len(self.get('text', '').split('<img ')) < 3:
+                s += f"<div class=\"image\"><img src=\"{self.get('Photo url')}\" align=\"middle\" width=\"600px\" ></div>\n"
+                s += f"<div class=\"text\"><h1>{self.get('Title', '')}</h1>\n{self.get('text', '')}<\div>"
+                return f"<div class=\"row\">{s}<\div>"
+            else:
+                s += f"<div ><img src=\"{self.get('Photo url')}\" align=\"middle\" width=\"600px\" ></div>\n"
+        if 'Title' in self.dat:
+            s = f"<h1>{self.get('Title', '')}</h1>\n" + s
+        s += self.get('text', '')
+        return f"<div class=\"main\">{s}<\div>"
+
+    @property
+    def tags(self):
+        return [t for t in self.get('Tags', '').strip().split(', ') if len(t) > 0]
+
+    @property
+    def date(self):
+        return dateutil.parser.parse(self['Date'])
+
+def loadEntries(path, entryType):
+    with open(path, encoding = 'utf8') as f:
+        entries = []
+        dat = {}
+        inBody = False
+        for e in f:
+            if inBody:
+                if e.startswith('Tags:'):
+                    k, *v = e.split(':')
+                    dat[k] = ':'.join(v).strip()
+                    inBody = False
+                    entries.append(Entry(dat))
+                    dat = {}
+                else:
+                    try:
+                        dat['text'] += e
+                    except KeyError:
+                        dat['text'] = e
+            elif len(e) == 1:
+                pass
+            else:
+                k, *v = e.split(':')
+                dat[k] = ':'.join(v).strip()
+                if entryType == 'texts' and e.startswith('Title:'):
+                    inBody = True
+                elif entryType == 'images' and e.startswith('Photo caption:'):
+                    inBody = True
+                    dat['text'] = ':'.join(v).strip()
+                elif entryType not in ['images', 'texts'] and e.startswith('Reblog name:'):
+                    inBody = True
+    return sorted(entries, key = lambda x : x.date, reverse = True)
+
+def listAccounts(path = tumblrsPath):
+    targets = []
+    for e in os.scandir(path):
+        for v in ['texts', 'images', 'answers', 'links']:
+            if os.path.isfile(os.path.join(e.path, f'{v}.txt')):
+                targets.append(e.name)
+                break
+    return targets
+
+class Account(object):
+    def __init__(self, name, inDash = False):
+        self.name = name
+        self.path = os.path.join(tumblrsPath, name)
+        self.inDash = inDash
+        self.postTypes = [e.name[:-4] for e in os.scandir(self.path) if e.name.endswith('.txt') and not e.name.startswith('video')]
+        self.currentType = 'texts' if 'texts' in self.postTypes else self.postTypes[0]
+        self.posts = {self.currentType : loadEntries(self._join(self.currentType + '.txt'), self.currentType) }
+        self._tags = {}
+
+        self.currentTag = 'None'
+
+    @property
+    def entries(self):
+        if self.currentType not in self.posts:
+            self.posts[self.currentType] = loadEntries(self._join(self.currentType + '.txt'), self.currentType)
+        return self.posts[self.currentType]
+
+    @property
+    def tags(self):
+        if self.currentType not in self._tags:
+            tags = {}
+            for i,e in enumerate(self.entries):
+                for t in e.tags:
+                    try:
+                        tags[t].append(e)
+                    except KeyError:
+                        tags[t] = [e]
+            self._tags[self.currentType] = tags
+        return self._tags[self.currentType]
+
+    def __len__(self):
+        return len(self.currentTagValues)
+
+    def __repr__(self):
+        return f"< Account {self.name} {len(self)} entries >"
+
+    def __getitem__(self, key):
+        return self.currentTagValues[key]
+
+    def _join(self, entry):
+        return os.path.join(self.path, entry)
+
+    def __call__(self, key):
+        return self.localizeHTML(self[key].html)
+
+    def sortedTags(self, countTuple = False, count = False):
+        if count:
+            return [f"{len(e)} {t}" for t, e in sorted(self.tags.items(), key = lambda x: len(x[1]), reverse = True)]
+        elif countTuple:
+            return [(len(e), t) for t, e in sorted(self.tags.items(), key = lambda x: len(x[1]), reverse = True)]
+        else:
+            return [t for t, e in sorted(self.tags.items(), key = lambda x: len(x[1]), reverse = True)]
+
+    @property
+    def currentTagValues(self):
+        if self.currentTag == 'None':
+            return self.entries
+        else:
+            return self.tags[self.currentTag]
+
+    def localizeHTML(self, target):
+        #Sreturn target
+        target = str(target)
+        soup = bs4.BeautifulSoup(target, 'lxml')
+        for i in soup.findAll('img'):
+            srcName = os.path.basename(i.get('src'))
+            testPath = self._join(srcName)
+            if os.path.isfile(testPath):
+                i.replaceWith(soup.new_tag("img", src=encode_image(testPath), **{"width" : i.get('width')}))
+            else:
+                testPath = sizedRE.sub('_1280.', srcName)
+                if os.path.isfile(testPath):
+                    i.replaceWith(soup.new_tag("img", src=encode_image(testPath), **{"width" : i.get('width')}))
+        if not self.inDash:
+            return str(soup)
+        else:
+            dashed = toDashHTML(soup.body)
+            return dashed
